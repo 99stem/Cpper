231a0abee3884182103bc9e824f288599e47449e
99stem <smartstn@gmail.com>
2013-04-29 21:08:30 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,16 @@ int main()
   cout << "Hello, What's your name?" << endl;
   cin >> name;
   cout << "Hello, " << name << endl;
+  
+  if(name == "99stem")
+  {
+    cout << "You are the owner :O" << endl;
+  }
+  else
+  {
+    cout << "You are not the owner :P" << endl;
+  }
+  
   return 0;
   
 }
